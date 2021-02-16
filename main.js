92fc798ddc8eda3866fc2b885cb717014e1d80dc
CostasAK <costas@kokke.eu>
2021-02-16 17:12:09 +0100

    corrected armor

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const or_list = {
   sunset: 'sunsetsin:current',
   exotic_weapons: 'is:exotic is:weapon is:dupelower -hawkmoon',
   armor:
-    '-crystocrene -dreambane -is:classitem -is:exotic ((is:legendary power:<=powerfulcap) or (is:rare power:<1310)) (basestat:total:<60 or basestat:custom:<40)',
+    '-crystocrene -dreambane -is:classitem -is:exotic ((is:legendary power:<=powerfulcap) or (is:rare power:<1310)) basestat:total:<60 basestat:custom:<40',
   class_items: 'is:classitem (sunsetsin:<=next or power:<powerfulcap)'
 }
 
